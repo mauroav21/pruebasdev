08c46435eb604e9b5a6ac40dc2d0e3ed37fbc603
CamaradeVideo <heiscorporation@gmail.com>
2025-07-25 17:26:58 +0000

    Base de Datos contenedor

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
-DockerFile
+FROM postgres:15
+COPY init.sql /docker-entrypoint-initdb.d/
--- init.sql
+++ init.sql
@@ -1,1 +1,5 @@
-Inicial
+CREATE TABLE users (ID SERIAL PRIMARY KEY, name VARCHAR(30), email VARCHAR(30));
+
+INSERT INTO users VALUES (1,'Jesus','Jesus@tecMilenio.mx');
+INSERT INTO users VALUES (2,'Moises','Moises@tecMilenio.mx');
+INSERT INTO users VALUES (3,'Noe','Noe@tecMilenio.mx');
